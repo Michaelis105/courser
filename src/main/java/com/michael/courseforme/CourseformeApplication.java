74d4257fdc5f137c027484274d4de72e9169881e
Michael Louie <michael.louie10@gmail.com>
2020-05-08 14:45:33 -0400

    Skeleton from spring initializer

---
+++ src/main/java/com/michael/courseforme/CourseformeApplication.java
@@ -0,0 +1,13 @@
+package com.michael.courseforme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseformeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseformeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/michael/courseforme/CourseformeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michael.courseforme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseformeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
